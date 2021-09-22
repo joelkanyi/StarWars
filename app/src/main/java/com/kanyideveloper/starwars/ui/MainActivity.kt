054f67b227b346f1b41fa86546eca3930a27056f
JoelKanyi <joelkanyi98@gmail.com>
2021-09-22 05:57:51 +0300

    added splash screen

--- app/src/main/java/com/kanyideveloper/starwars/ui/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/starwars/ui/MainActivity.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.kanyideveloper.starwars.R
 import com.kanyideveloper.starwars.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,17 +17,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        //Setup Top Back post_style_button and hide the arrow for some fragments
+        appBarConfiguration = AppBarConfiguration
+            .Builder(
+                R.id.charactersFragment,
+                R.id.splashFragment
+            ).build()
+
         setSupportActionBar(binding.toolbarMain)
-        setupActionBarWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
